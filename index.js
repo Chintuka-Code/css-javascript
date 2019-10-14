5ec41c011c59a0c046d792c732fbb845460ffd23
sabban <mohmadsabban123@gmail.com>
2019-10-14 22:39:49 +0530

    slider complete

--- index.js
+++ index.js
@@ -21,5 +21,24 @@ function out(imgName)
  image.style.removeProperty("background-color");
 */
   image.classList.remove("newclass");
+}
 
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function next(n) {
+  showDivs(slideIndex += n);
+}
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("image");
+  var lenght = x.length;
+  document.getElementById("demo").innerHTML="Value of n:-"+n+",    length:-"+lenght;
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
 }
